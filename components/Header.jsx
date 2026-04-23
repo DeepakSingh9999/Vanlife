f184bcfd110af67c9fd4707df3653fb5bd621e10
Deepak Singh <Deepak.walton@proton.me>
2026-04-23 16:01:46 +0530

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+import { Link, NavLink, useNavigate } from "react-router-dom"
+import imageUrl from "/assets/images/avatar-icon.png"
+
+export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    const navigate = useNavigate();
+
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+        navigate("/")
+    }
+
+    return (
+        <header className="header-container">
+            <Link className="site-logo" to="/">#VanLife</Link>
+            <nav>
+                <NavLink
+                    to="/host"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Host
+                </NavLink>
+                <NavLink
+                    to="/about"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    to="/vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
+                <Link to="login" className="login-link">
+                    <img
+                        src={imageUrl}
+                        className="login-icon"
+                    />
+                </Link>
+                <button onClick={fakeLogOut} className="logout-button">⏻</button>
+            </nav>
+        </header>
+    )
+}
